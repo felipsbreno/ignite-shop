41112965fc480f4e4da14da0abae7e5234ea4663
felipsbreno <brendo.filipe2050@gmail.com>
2022-12-06 00:11:37 -0300

    criação de novas paginas, configurando o documento e fonte

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 export default function Home() {
-  return <h1>Ignite Shop</h1>;
+  return <Link href="/success">success</Link>;
 }
---
+++ src/pages/slug/[slug].tsx
@@ -0,0 +1,5 @@
+import Link from 'next/link';
+
+export default function Success() {
+  return <Link href="/">Product</Link>;
+}
