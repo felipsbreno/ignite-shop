11ddd7871cb73d27e9d8378862ad7d6745bbb0ba
felipsbreno <brendo.filipe2050@gmail.com>
2022-12-06 00:28:34 -0300

    adicionando e configurando o stitches

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,16 @@
 import Link from 'next/link';
+import { styled, theme } from '../styles';
+
+const Button = styled('button', {
+  backgroundColor: theme.colors.rocketseat,
+});
 
 export default function Home() {
-  return <Link href="/success">success</Link>;
+  return (
+    <>
+      <Link href="/success">success</Link>
+
+      <Button>Olá</Button>
+    </>
+  );
 }
---
+++ src/styles/index.ts
@@ -0,0 +1,17 @@
+import { createStitches } from '@stitches/react';
+
+export const {
+  config,
+  styled,
+  css,
+  keyframes,
+  theme,
+  getCssText,
+  createTheme,
+} = createStitches({
+  theme: {
+    colors: {
+      rocketseat: '#8257e6',
+    },
+  },
+});
