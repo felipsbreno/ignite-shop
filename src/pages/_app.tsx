4bbbfb22dd12e03e5bf95daa9f0fcf068399227b
felipsbreno <brendo.filipe2050@gmail.com>
2022-12-06 22:27:20 -0300

    add novos produtos a atualizando os arquivos index e app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,20 @@
 import { AppProps } from 'next/app';
-import { globalStyles } from '../styles/global';
+import Image from 'next/image';
 
-import logoImg from '../assets/logo.svg';
+import { globalStyles } from '../styles/global';
 import { Container, Header } from '../styles/pages/app';
 
-import Image from 'next/image';
+import logoImg from '../assets/logo.svg';
 
 globalStyles();
 
-function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <Image src={logoImg} alt="" />
+        <Image src={logoImg} alt="imagem da logo" />
       </Header>
-
       <Component {...pageProps} />
     </Container>
   );
 }
-
-export default App;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,53 @@
-import Link from 'next/link';
-import { styled, theme } from '../styles';
+import Image from 'next/image';
+import { HomeContainer, Product } from '../styles/pages/home';
 
-const Button = styled('button', {
-  backgroundColor: theme.colors.gray900,
-});
+import camiseta1 from '../assets/1.png';
+import camiseta2 from '../assets/2.png';
+import camiseta3 from '../assets/3.png';
 
 export default function Home() {
   return (
-    <>
-      <Link href="/success">success</Link>
+    <HomeContainer>
+      <Product>
+        <Image
+          src={camiseta1.src}
+          width={520}
+          height={480}
+          alt="Image do produto 1"
+        />
 
-      <Button>Olá</Button>
-    </>
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image
+          src={camiseta2.src}
+          width={520}
+          height={480}
+          alt="Image do produto 2"
+        />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image
+          src={camiseta3.src}
+          width={520}
+          height={480}
+          alt="Image do produto 3"
+        />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   );
 }
